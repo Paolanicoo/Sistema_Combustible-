668caf1fa1c64e9f06dfe8dffd1a8644b5a99e18
Paola Merlo <paolamerlo15@gmail.com>
2025-04-23 07:36:05 -0600

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Gestión de Combustible</title>
+    <title>Gestión de Combustible</title>
+    <link rel="icon" href="{{ asset('img/icono.PNG') }}" type="image/PNG">
+    
+    <!-- Otros estilos y scripts que ya tenías en el head -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@700&display=swap" rel="stylesheet">
